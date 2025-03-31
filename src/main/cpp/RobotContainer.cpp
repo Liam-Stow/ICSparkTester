e86a5a68a0018600bb6e2e71c217c2e8af5090d4
Liam Stow <liam.stow@outlook.com>
2025-03-31 16:18:16 +1100

    bind to buttons

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -1,16 +1,17 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 #include "RobotContainer.h"
-
 #include <frc2/command/Commands.h>
+
+#include "subsystems/Arm.h"
 
 RobotContainer::RobotContainer() {
   ConfigureBindings();
 }
 
-void RobotContainer::ConfigureBindings() {}
+void RobotContainer::ConfigureBindings() {
+  _controller.A().WhileTrue(Arm::GetInstance().DriveWithDutyCycle(0.8));
+  _controller.B().WhileTrue(Arm::GetInstance().DriveWithDutyCycle(-0.5));
+
+}
 
 frc2::CommandPtr RobotContainer::GetAutonomousCommand() {
   return frc2::cmd::Print("No autonomous command configured");
