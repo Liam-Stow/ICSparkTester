b638c61519bded1f89a9f92d069236777010f123
Liam Stow <liam.stow@outlook.com>
2025-05-13 18:54:53 +1000

    controller whileTrue -> onTrue

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -7,30 +7,30 @@ RobotContainer::RobotContainer() {
 
 void RobotContainer::ConfigureBindings() {
   // Arm
-  _armController.A().WhileTrue(_arm.MaxMotionTo(0_deg));
-  _armController.B().WhileTrue(_arm.WPIProfileTo(90_deg));
-  _armController.X().WhileTrue(_arm.PIDTo(180_deg));
-  _armController.Y().WhileTrue(_arm.DriveWithDutyCycle(-1));
+  _armController.A().OnTrue(_arm.MaxMotionTo(0_deg));
+  _armController.B().OnTrue(_arm.WPIProfileTo(90_deg));
+  _armController.X().OnTrue(_arm.PIDTo(180_deg));
+  _armController.Y().OnTrue(_arm.DriveWithDutyCycle(-1));
 
   // Elevator
-  _elevatorController.A().WhileTrue(_elevator.MaxMotionTo(0.5_m));
-  _elevatorController.B().WhileTrue(_elevator.WPIProfileTo(0_m));
-  _elevatorController.X().WhileTrue(_elevator.PIDTo(0.5_m));
-  _elevatorController.Y().WhileTrue(_elevator.DriveWithDutyCycle(0.5));
+  _elevatorController.A().OnTrue(_elevator.MaxMotionTo(0.5_m));
+  _elevatorController.B().OnTrue(_elevator.WPIProfileTo(0_m));
+  _elevatorController.X().OnTrue(_elevator.PIDTo(0.5_m));
+  _elevatorController.Y().OnTrue(_elevator.DriveWithDutyCycle(0.5));
 
   // Feeder
-  _feederController.A().WhileTrue(_feeder.FeedIn());
-  _feederController.B().WhileTrue(_feeder.FeedOut());
+  _feederController.A().OnTrue(_feeder.FeedIn());
+  _feederController.B().OnTrue(_feeder.FeedOut());
 
   // Flywheel
-  _flywheelController.A().WhileTrue(_flywheel.SpinAt(1000_rpm));
-  _flywheelController.B().WhileTrue(_flywheel.SpinAt(-1000_rpm));
+  _flywheelController.A().OnTrue(_flywheel.SpinAt(1000_rpm));
+  _flywheelController.B().OnTrue(_flywheel.SpinAt(-1000_rpm));
 
   // Turret
-  _turretController.A().WhileTrue(_turret.MaxMotionTo(0_deg));
-  _turretController.B().WhileTrue(_turret.WPIProfileTo(90_deg));
-  _turretController.X().WhileTrue(_turret.PIDTo(180_deg));
-  _turretController.Y().WhileTrue(_turret.DriveWithDutyCycle(-1));
+  _turretController.A().OnTrue(_turret.MaxMotionTo(0_deg));
+  _turretController.B().OnTrue(_turret.WPIProfileTo(90_deg));
+  _turretController.X().OnTrue(_turret.PIDTo(180_deg));
+  _turretController.Y().OnTrue(_turret.DriveWithDutyCycle(-1));
 
 }
 
